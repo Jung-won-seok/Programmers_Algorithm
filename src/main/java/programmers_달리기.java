dfc43f5d2f89a74f03a2bda12f46de8b4fc40fc9
Jung-won-seok <ukksj0621@naver.com>
2024-06-06 20:26:16 +0900

    Initial commit

---
+++ src/main/java/programmers_달리기.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class programmers_달리기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] answer = {};
+        return ;
+    }
+}
