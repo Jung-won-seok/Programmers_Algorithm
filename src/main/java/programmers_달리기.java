98de4b6513f1d11059adaa1f22ed96e89eac8997
Jung-won-seok <ukksj0621@naver.com>
2024-06-08 20:27:10 +0900

    달리기 경주

--- src/main/java/programmers_달리기.java
+++ src/main/java/programmers_달리기.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class programmers_달리기 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] answer = {};
-        return ;
+        int A = sc.nextInt();
+        int[] num = new int[A];
     }
 }
