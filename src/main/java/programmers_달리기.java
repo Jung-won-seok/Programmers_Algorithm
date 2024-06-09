d4777ee5c6a446b87849282f976bc7e95371df10
Jung-won-seok <ukksj0621@naver.com>
2024-06-09 21:34:12 +0900

    달리기 경주

--- src/main/java/programmers_달리기.java
+++ src/main/java/programmers_달리기.java
@@ -1,7 +1,9 @@
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class programmers_달리기 {
     public static void main(String[] args) {
+        HashSet<Integer> hash = new HashSet<>();
         Scanner sc = new Scanner(System.in);
         int A = sc.nextInt();
         int[] num = new int[A];
